31c4c7a95d111414fa84d342fbf11cc54c17e83e
rajaonera <hugueszeus@gmail.com>
2024-06-19 22:04:41 +0300

    Sprint05 - ajout gestion exception

--- src/mg/framework/controller/FrontController.java
+++ src/mg/framework/controller/FrontController.java
@@ -1,144 +1,98 @@
 package mg.framework.controller;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import mg.framework.models.Mapping;
+import mg.framework.utils.ServletManager;
+
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
-
-import mg.framework.models.Mapping;
-import mg.framework.models.ModelView;
-import mg.framework.utils.ServletManager;
-import mg.framework.utils.Utils;
 
 public class FrontController  extends HttpServlet{
-    
-    // Sprint 01
-    private ArrayList<Class<?>> classController;
-    HashMap<String, Mapping> mappingUrls = new HashMap<>();
+    private ArrayList<Class<?>> classController = new ArrayList<>();
+    private HashMap<String,Mapping> controllerAndMethod = new HashMap<>(); 
 
     public ArrayList<Class<?>> getClassController() {
         return classController;
     }
-
     public void setClassController(ArrayList<Class<?>> classController) {
         this.classController = classController;
     }
-
-
-    // Sprint 00
-    public void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
-        PrintWriter out = response.getWriter();
-        String url =  request.getRequestURI();
-        out.println("URL : " + url);
-        this.showMappingsUrlsAndMethods(out,url);
-        this.executeMethodController(url, request, response);
-
+    public HashMap<String, Mapping> getControllerAndMethod() {
+        return controllerAndMethod;
     }
-
-    public HashMap<String, Mapping> getMappingUrls() {
-        return mappingUrls;
+    public void setControllerAndMethod(HashMap<String, Mapping> controllerAndMethod) {
+        this.controllerAndMethod = controllerAndMethod;
     }
-    public void getMappingUrls(HashMap<String, Mapping> mappingUrl) {
-        this.mappingUrls = mappingUrl;
-    }
-
-    public void setMappingUrls(String url, Mapping method) {
-        this.mappingUrls.put(url, method);
+    public void addControllerAndMethod(String url, Mapping method) {
+        this.controllerAndMethod.put(url, method);
     }
 
     public void initController() {
         try {
-            String packageCtrl = this.getInitParameter("smooth.mg.controller");
+            String packageCtrl = this.getInitParameter("packageName");
             this.setClassController(ServletManager.getControllerClasses(packageCtrl));
         } catch (Exception e) {
-            System.out.printf(e.getLocalizedMessage());
+            e.printStackTrace();
         }
     }
 
-    public void init() {
+    public void init() throws ServletException{
         this.initController();
         try {
-            ServletManager.getControllerMethod(this.getClassController(),this.getMappingUrls());
-                
-        } catch (Exception e) {
-            System.out.println(e.getLocalizedMessage());
-        }
-    }
-
-    public void showMappingsUrlsAndMethods(PrintWriter out, String url) {
-        Mapping map = ServletManager.getUrl(this.getMappingUrls(), url);
-        try {
+            HashMap<String,Mapping> map = ServletManager.getControllerMethod(this.getClassController());
+            
             if (map != null) {
-                out.println("Controller Name : " + map.getClassName());
-                out.println("Method Name : " + map.getMethodName());
-            } else {
-                out.println("No method found!");
+                this.setControllerAndMethod(map);
             }
         } catch (Exception e) {
-            out.println(e.getMessage());
+            throw new RuntimeException(e);
         }
     }
 
-    public void dispatchModelView(ModelView modelView, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        for (Map.Entry<String,Object> data : modelView.getData().entrySet()){
-            String varName = data.getKey();
-            Object varValue = data.getValue();
-            request.setAttribute(varName,varValue);
-        }
-        RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/"+modelView.getUrl());
-        dispatcher.forward(request,response);
-    }
+    public void showController(PrintWriter out) {
+        ArrayList<Class<?>> classes = new ArrayList<>();
+        if (classes != null && this.getClassController() != null && !this.getClassController().isEmpty()) {
+            classes = this.getClassController();
 
-    public void executeMethodController(String url, HttpServletRequest request, HttpServletResponse response) throws IOException , ServletException {
-        PrintWriter out = response.getWriter();
-        
-        String packageCtrl = this.getInitParameter("packageName");
-        Mapping map = ServletManager.getUrl(this.getMappingUrls(), url);
-        try {
-            if (map != null) {
-                String className = map.getClassName();
-                String methodName = map.getMethodName();
-                String classPath = packageCtrl+"."+className;
-
-                Class<?> controllerClass = Class.forName(classPath);
-                Method controllerMethod = Utils.getMethod(controllerClass, methodName);
-
-                if (controllerMethod.getReturnType() == String.class || controllerMethod.getReturnType() == ModelView.class) {
-                    Object ctrlObj = controllerClass.newInstance(); 
-                    out.println("Return: "+ controllerMethod.getReturnType());
-                    if (controllerMethod.getReturnType() == String.class) {
-                        String methodReturn =  (String) Utils.executeSimpleMethod(ctrlObj, methodName);
-                    
-                        out.print("After executing the "+ methodName +" method in the "+ className +".class, this method returned the value: ");
-                        out.println(methodReturn);
-                    } 
-                    if (controllerMethod.getReturnType() == ModelView.class) {
-                        ModelView modelView = (ModelView) Utils.executeSimpleMethod(ctrlObj, methodName);
-                        this.dispatchModelView(modelView, request, response);
-                    }
-                } else {
-                    out.println("The return type of method "+ methodName + " in " + className + ".class is invalid!" );
-                }
-
-            } else {
-                out.println("No method found!");
+            out.println("Controllers : ");
+            for (Class<?> classe : classes) {
+                out.println("- " + classe.getSimpleName());
             }
+        }
+    }
+
+    public void showControllerAndMethod(PrintWriter out, String url) throws Exception{
+        try {
+            Mapping map = ServletManager.getUrl(this.getControllerAndMethod(), url);
+            out.println("Controller Name : " + map.getClassName());
+            out.println("Method Name : " + map.getMethodName());
         } catch (Exception e) {
             out.println("Error : " + e.getMessage());
         }
     }
+
+    public void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+        PrintWriter out = response.getWriter();
+        String packageCtrl = this.getInitParameter("packageName");
+        String url =  request.getRequestURI();
+        out.println("URL : " + url);
+
+        try {
+            ServletManager.executeMethodController(url, request, response, packageCtrl, this.getControllerAndMethod());
+        } catch (Exception e) {
+            out.println("Error : " + e.getMessage());
+        }
+    }
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
         this.processRequest(request,response);
     }
-
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
         this.processRequest(request,response);
--- temp/ServletManager.java
+++ temp/ServletManager.java
@@ -1,48 +1,65 @@
 package mg.framework.utils;
 
-import mg.framework.annotations.Controller;
-import mg.framework.annotations.Get;
-import mg.framework.models.Mapping;
-
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mg.framework.annotations.Controller;
+import mg.framework.annotations.Get;
+import mg.framework.exception.DuplicateException;
+import mg.framework.exception.PackageException;
+import mg.framework.exception.ReturnException;
+import mg.framework.exception.UrlNotFoundException;
+import mg.framework.models.Mapping;
+import mg.framework.models.ModelView;
+
+
+@SuppressWarnings("deprecation")
 public class ServletManager {
-    public static ArrayList<Class<?>> getControllerClasses(String packageName) throws ClassNotFoundException, IOException {
+    public static ArrayList<Class<?>> getControllerClasses(String packageName) throws Exception {
         ArrayList<Class<?>> classes = Utils.getClasses(packageName);
         ArrayList<Class<?>> result = new ArrayList<Class<?>>();
-
+        
         for(Class<?> classe : classes) {
             if (classe.isAnnotationPresent(Controller.class)) {
                 result.add(classe);
-            }
+            } 
         }
-
+        if (result.size() <= 0) {
+            throw new PackageException(packageName);
+        }
         return result;
     }
 
-    public static void getControllerMethod(ArrayList<Class<?>> classes,HashMap<String,Mapping>urlsHashMap) throws Exception {
+    public static HashMap<String,Mapping> getControllerMethod(ArrayList<Class<?>> classes) throws Exception {
+        HashMap<String,Mapping> result = new HashMap<>();
         if (classes != null) {
             for(Class<?> classe : classes) {
                 ArrayList<Method> methods = Utils.getListMethod(classe);
                 for (Method method : methods) {
                     if (method.isAnnotationPresent(Get.class)) {
-                        String url = ( method.getAnnotation(Get.class)).value();
-                        if (urlsHashMap.get(url)==null) {
+                        String url = ((Get) method.getAnnotation(Get.class)).value();
+                        if (result.get(url)==null) {
                             Mapping mapping = new Mapping(classe.getSimpleName(),method.getName());
-                            urlsHashMap.put(url, mapping);
+                            result.put(url, mapping);
                         } else {
-                            throw new Exception("Duplicate annotation : "+ url +" in multiple methods!");
+                            throw new DuplicateException();
                         }
                     }
                 }
             }
         }
+        return result;
     }
 
-    public static Mapping getUrl(HashMap<String, Mapping> maps, String url) {
+    public static Mapping getUrl(HashMap<String, Mapping> maps, String url) throws Exception {
         Mapping result = null;
         String[] path = url.split("/");
         String newUrl = new String();
@@ -53,11 +70,53 @@ public class ServletManager {
             }
             newUrl = path[i] + newUrl;
             Mapping map = maps.get(newUrl);
-
+            
             if (map != null) {
                 result = map;
             }
+        } 
+        if (result == null) {
+            throw new UrlNotFoundException();
         }
         return result;
+    }
+
+    public static void executeMethodController(String url, HttpServletRequest request, HttpServletResponse response, String packageName, HashMap<String,Mapping> controllerAndMethod) throws Exception {
+        PrintWriter out = response.getWriter();        
+        Mapping map = ServletManager.getUrl(controllerAndMethod, url);
+
+        String className = map.getClassName();
+        String methodName = map.getMethodName();
+        String classPath = packageName+"."+className;
+        
+        Class<?> controllerClass = Class.forName(classPath);
+        Method controllerMethod = Utils.getMethod(controllerClass, methodName);
+
+        if (controllerMethod.getReturnType() == String.class || controllerMethod.getReturnType() == ModelView.class) {
+            Object ctrlObj = controllerClass.newInstance(); 
+            
+            if (controllerMethod.getReturnType() == String.class) {
+                String methodReturn =  (String) Utils.executeSimpleMethod(ctrlObj, methodName);
+            
+                out.print("After executing the "+ methodName +" method in the "+ className +".class, this method returned the value: ");
+                out.println(methodReturn);
+            } 
+            if (controllerMethod.getReturnType() == ModelView.class) {
+                ModelView modelView = (ModelView) Utils.executeSimpleMethod(ctrlObj, methodName);
+                dispatchModelView(modelView, request, response);
+            }
+        } else {
+            throw new ReturnException(methodName, className);
+        }
+    }
+
+    public static void dispatchModelView(ModelView modelView, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        for (Map.Entry<String,Object> data : modelView.getData().entrySet()){
+            String varName = data.getKey();
+            Object varValue = data.getValue();
+            request.setAttribute(varName,varValue);
+        }
+        RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/"+modelView.getUrl());
+        dispatcher.forward(request,response);
     }
 }
